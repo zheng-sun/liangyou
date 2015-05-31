b6b72b8ef69b5a352dbd2c7d5d4143b84bb873f9
xiaolan2030 <774613288@qq.com>
2015-05-31 10:29:02 +0800

    test

--- api.php
+++ api.php
@@ -6,6 +6,7 @@
  * @license				http://www.phpcms.cn/license/
  * @lastmodify			2010-7-26
  */
+
 define('PHPCMS_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 include PHPCMS_PATH.'phpcms/base.php';
 $param = pc_base::load_sys_class('param');
